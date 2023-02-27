a6973572db61164420be37d844a1c89b6072c4db
Kawã Vinicius Batista <88722824+batistakv@users.noreply.github.com>
2023-02-26 21:24:44 -0300

    so vai ter esse commit

---
+++ ATV.js
@@ -0,0 +1,47 @@
+function validarCPF(cpf) {
+    cpf = cpf.replace(/\D+/g, '');
+  
+
+    if (cpf.length !== 11) {
+      return false;
+    }
+  
+
+    if (/^(\d)\1{10}$/.test(cpf)) {
+      return false;
+    }
+  
+
+    let soma = 0;
+
+    for (let i = 0; i < 9; i++) {
+      soma += parseInt(cpf.charAt(i)) * (10 - i);
+    }
+
+    let digito1 = 11 - (soma % 11);
+
+    if (digito1 > 9) {
+      digito1 = 0;
+    }
+  
+    soma = 0;
+
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(cpf.charAt(i)) * (11 - i);
+    }
+
+    let digito2 = 11 - (soma % 11);
+
+    if (digito2 > 9) {
+      digito2 = 0;
+    }
+  
+
+    if (cpf.charAt(9) != digito1 || cpf.charAt(10) != digito2) {
+      return false;
+    }
+  
+    
+    return true;
+  }
+  
